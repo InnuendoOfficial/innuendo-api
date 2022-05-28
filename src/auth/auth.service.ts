dbc94aaf0f686d94b846ed9586bbd01f6ca6f009
tristanB12 <tristan.bourgeois@epitech.eu>
2022-05-28 14:41:41 +0200

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { toUnicode } from 'punycode';
+
+@Injectable()
+export class AuthService {
+    constructor(private prisma: PrismaService) {}
+
+    async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            });
+            delete user.hash;
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials already taken');
+                }
+            }
+            throw error;
+        }
+    }
+
+    async login(dto: AuthDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        });
+        if (!user) throw new ForbiddenException('Incorrect email');
+        if (!await argon.verify(user.hash, dto.email)) throw new ForbiddenException('Incorrect password');
+        delete user.hash;
+        return user;
+    }
+}
