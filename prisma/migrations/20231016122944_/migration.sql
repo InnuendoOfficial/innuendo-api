0f256f446fa7101c7265912716113f9fd4180f11
tristanB12 <tristan.bourgeois@epitech.eu>
2023-10-16 14:29:52 +0200

    migration

---
+++ prisma/migrations/20231016122944_/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [bool] on the enum `UnitMeasure` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "UnitMeasure_new" AS ENUM ('int', 'string', 'boolean');
+ALTER TABLE "SymptomType" ALTER COLUMN "unit_measure" TYPE "UnitMeasure_new" USING ("unit_measure"::text::"UnitMeasure_new");
+ALTER TYPE "UnitMeasure" RENAME TO "UnitMeasure_old";
+ALTER TYPE "UnitMeasure_new" RENAME TO "UnitMeasure";
+DROP TYPE "UnitMeasure_old";
+COMMIT;
